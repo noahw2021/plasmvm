763c28f083107308eaf70f94f47005948dd4c160
Noah Wooten <noah.wooten@icloud.com>
2022-02-04 14:37:50 -0500

    begin stack operations

--- plasmvm/mmu/stack.c
+++ plasmvm/mmu/stack.c
@@ -8,10 +8,16 @@
 #include "mmu.h"
 
 word mmui_stackpull(void) {
-	
+	u64* Stack = mmu_translate(ctx->sp, _ACCESS_READ);
+	u64 Return = Stack[0];
+	ctx->sp += 8;
+	return Return;
 }
 byte mmui_stackpullbyte(void) {
-	
+	byte* Stack = mmu_translate(ctx->sp, _ACCESS_READ);
+	byte Return = Stack[0];
+	ctx->sp += 1;
+	return Return;
 }
 
 void mmui_stackput(word Value) {
