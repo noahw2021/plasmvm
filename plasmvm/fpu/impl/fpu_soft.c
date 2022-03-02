67af4f750d9a751f3b2e854d2fb99de799dbfb07
Noah Wooten <noah.wooten@icloud.com>
2022-03-02 14:03:39 -0500

    implement more soft flu

--- plasmvm/fpu/impl/fpu_soft.c
+++ plasmvm/fpu/impl/fpu_soft.c
@@ -9,8 +9,6 @@
 #include <math.h>
 #include "../fpu.h"
 
-
-
 x64 fpud_pow(x64 Power, x64 Source) {
 	x64 Return = 1.0;
 	x64 Fraction, Fraction2;
@@ -28,29 +26,61 @@ x64 fpud_pow(x64 Power, x64 Source) {
 }
 
 x64 fpud_rooti(int Root, x64 Source) {
-	
+	return fpui_rooti(Source, Root);
 }
 
 x64 fpud_expi(int Exponent, x64 Source) {
-
+	x64 Return = 1;
+	for (int i = 0; i < Exponent; i++) {
+		Return *= Source;
+	}
+	return Return;
 }
 
 x64 fpud_mod(x64 Divisor, x64 Source) {
-
+	x64 Return = Source;
+	while (1) {
+		if (Return > Divisor)
+			Return -= Divisor;
+		if (Return < Divisor)
+			return Return;
+	}
 }
 
 x32 fpus_pow(x32 Power, x32 Source) {
-
+	x64 Return = 1.0;
+	x64 Fraction, Fraction2;
+	modf(Power, &Fraction);
+	if (Fraction == 0) {
+		for (int i = 0; i < Power; i++) {
+			Return *= Source;
+		}
+		return Return;
+	} else {
+		x64 Fraction3 = modf(Fraction, &Fraction2);
+		u64 Root = (u64)(1.0 / (x64)Fraction3);
+		return fpud_rooti((int)Root, fpud_pow(Fraction2, Source));
+	}
 }
 
 x32 fpus_rooti(int Root, x32 Source) {
-
+	return (x32)fpui_rooti((x64)Source, Root);
 }
 
 x32 fpus_expi(int Exponent, x32 Source) {
-
+	x32 Return = 1;
+	for (int i = 0; i < Exponent; i++) {
+		Return *= Source;
+	}
+	return Return;
 }
 
 x32 fpus_mod(x32 Divisor, x32 Source) {
-
+	x32 Return = Source;
+	while (1) {
+		if (Return > Divisor)
+			Return -= Divisor;
+		if (Return < Divisor)
+			return Return;
+	}
 }
